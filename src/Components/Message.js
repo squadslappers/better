802e73ac45a755d153e83521c5122a4f074b1e81
Jake <jacob.w.moreno@gmail.com>
2020-09-27 17:51:55 -0600

    Almost done.

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -3,7 +3,8 @@ import React, {useState} from 'react';
 const Message = (props) => {
   return(<div className='message'>
     <div className='message__inner'>
-      
+      <h1>Enter a Message Asking for Advice</h1>
+      <textarea className='message__input'/>
     </div>
   </div>)
 }
